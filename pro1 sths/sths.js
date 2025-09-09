0016176016947114c4857d562c2ed746d5c23327
Kabunga Charles <kcjesuschrist513@gmail.com>
2025-09-09 10:53:58 +0300

    update.html

---
+++ pro1 sths/sths.js
@@ -0,0 +1,5 @@
+alert("It's your lucky day to be on this platform. STANDARD HILL SCHOOL WELCOMES YOU!");
+
+prompt("Enter your email please!");
+
+confirm("Is the email you have inserted the true one?");
