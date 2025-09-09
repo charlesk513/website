154709edb61b5951c1b87e88774b7ebcc7b25853
Kabunga Charles <kcjesuschrist513@gmail.com>
2025-09-09 11:24:14 +0300

    update epta

---
+++ pro1 epta/epta.js
@@ -0,0 +1,9 @@
+alert("Welcome to the platform of Epta Gas distributors Uganda Limited. We shall be pleased if we contentfuly serve you!");
+
+confirm("Allow this website to send you notifications about our products.");
+
+let pleaseAllow = 'confirm("Allow this website to send you notifications about our products.");'
+
+if (pleaseAllow == true){
+    document.write('prompt("Please enter your email address!");');
+    }
